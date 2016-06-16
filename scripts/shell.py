347e6dc1e24190a4ea63dbddec022fe346cffabe
Zac <zacdblanco@gmail.com>
2016-06-16 16:17:28 -0700

    [#2] - Added basic shell usage class
    Basic shell usage class for running bash commands or scripts
    
    Resolves #2

---
+++ scripts/shell.py
@@ -0,0 +1,46 @@
+import os, subprocess
+
+# The shell class defines just a few functions which can make executing commands easier
+# run
+# 	This command has two possible ways to be called
+# 	(command) which is just a single line with all arguments
+# 	(command, args) which simply just joins a string with the command and arguments
+# 
+# It is also possible to change the current working directory (cwd)
+# for commands which are sensitive to file locations
+# (Unfortunately 'cd' doesn't work)
+
+# If no arguments are passed for args, then it is assumed to be a string of length 0
+# If no arguments are passed to the constructor we assume default cwd
+
+class Shell:
+	
+	cwd = ''
+
+	def run(self, command, args=''):
+		
+		if len(args) > 0:
+			command = ' '.join([command, args.join(' ')])
+		
+		path = os.getcwd()
+		if len(self.cwd) > 0:
+			process = subprocess.Popen(command, shell=True, cwd=path, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+		else:
+			process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+		output = process.communicate()
+		return output
+
+
+	# set_cwd
+	# equivalent to doing a 'cd' command at the command line
+	# Error if the directory doesn't exist
+	def set_cwd(self, new_cwd):
+		if not os.path.exists(new_cwd):
+			raise IOError(' '.join([self.cwd, 'does not exist']))
+		else:
+			self.cwd = new_cwd
+
+	def __init__(self, wd=''):
+		if not wd == '':
+			self.set_cwd(wd)
+		
