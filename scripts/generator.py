8501dd5e50f464be77ef9af84988990062f4944e
zblanco <zblanco@apache.org>
2016-07-01 10:00:08 -0700

    first run of generator with string datum implementation

---
+++ scripts/generator.py
@@ -0,0 +1,133 @@
+import json, config, random
+from abc import abstractmethod
+
+
+class DataGenerator():
+	
+	def __init__(self, schema, seed=''):
+		self.data_fields = []
+		self.schema = schema
+		if not seed == '':
+			random.seed(seed)
+		self.check_schema(schema)
+				
+	# Returns true/false whether or not the schema is valid
+	# Raises an exception?
+	def check_schema(self, schema):
+		path = config.get_conf_dir() + schema
+
+		with open(path) as data_file:
+			conf = json.load(data_file)
+			
+			if not type(conf) == list:
+				raise TypeError('Root of JSON Schema is not a list')
+			
+			for field in conf:
+				if not 'fieldName' in field:
+					raise KeyError('Could not find \'fieldName\' in field of schema: ' + schema )
+					
+				if not 'type' in field:
+					raise KeyError('Could not find \'type\' in field of schema: ' + schema)
+				
+				
+				field_type = field['type']
+				datum = AbstractDatum(field)
+				if 'string' == field_type:
+					datum = StringDatum(field)
+				else:
+					raise RuntimeError('Field type was not found. Please change the field type or implement the datum correctly')
+					
+				datum.check() # Check to make sure the field has necessary attributes	
+#				print("FIELD LENGTH: " + str(len(self.data_fields)))
+				self.data_fields.append(datum)
+		
+	def generate(self):
+		data = []
+		for field in self.data_fields:
+			val = field.generate(random)
+			data.append(val)
+		return data
+
+
+class AbstractDatum(object):
+		
+	def __init__(self, field):
+		self.field_name = field['fieldName']
+		self.field = field
+		self.check_for_key('fieldName')
+	
+	def check_for_key(self, key_name):
+		if not key_name in self.field:
+			raise KeyError('Missing key: ' + key_name + ' in ' + self.field_name)
+		else:
+			return True
+	
+	# A method to determine whether or not the schema object has the necessary fields.
+	@abstractmethod
+	def check(self):
+		raise NotImplementedError('AbstractDatum: This method should have been implemented by a sublcass')
+	
+	@abstractmethod
+	def generate(self, rand):
+		raise NotImplementedError('AbstractDatum This method should have been implemented by a sublcass')
+
+class StringDatum(AbstractDatum):
+	values = []
+	def __init__(self, field):
+		AbstractDatum.__init__(self, field)
+		#calculate CDF if necessary
+		self.values = [] # list will be sorted by cumulative probability
+		if type(self.field['values']) == dict:
+			csum = 0
+			for key in self.field['values']:
+				prob = self.field['values'][key]
+				csum += prob
+				entry = {}
+				entry['key'] = key
+				entry['prob'] = csum
+				self.values.append(entry)			
+		
+	def check(self):
+		self.check_for_key('type')
+		assert (self.field['type'] == 'string')
+		val_type = type(self.field['values'])  
+		assert (val_type == list or val_type == dict)
+		
+	def generate(self, rand):
+		if type(self.field['values']) == list:
+			num_items = len(self.field['values'])
+			index = rand.randint(0, num_items - 1)
+			return self.field['values'][index]
+		elif type(self.field['values']) == dict:
+			val = random.random()
+			for i in range(len(self.values)):
+				if val < self.values[i]['prob']:
+					return self.values[i]['key']			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
---
+++ tests/test_generator.py
@@ -0,0 +1,96 @@
+import unittest, json, mock
+from env import scripts
+from mock import Mock
+from scripts.generator import DataGenerator
+
+class TestDataGenerator(unittest.TestCase):
+	
+	@mock.patch('scripts.config.get_conf_dir', return_value='res/')
+	def test_string_datum_good(self, mock1):
+		gen = DataGenerator('char_gen_good.json', seed='1234567890')
+		test_num = 500
+		data = []
+		for i in range(test_num):
+			data.append(gen.generate())
+		
+		occ = {}
+		total = 0
+		for items in data:
+			for char in items:
+				total += 1
+				if char in occ:
+					occ[char] += 1
+				else:
+					occ[char] = 1
+						
+#		for key in occ.keys():
+#			print("KEY: " + key + " | OCC: " + str(occ[key]) + " | PROB: " + str( occ[key]/float(test_num) ))
+		
+		assert total == 4*test_num
+		assert occ['a'] == 138
+		assert occ['c'] == 111
+		assert occ['b'] == 127
+		assert occ['e'] == 132
+		assert occ['d'] == 124
+		assert occ['g'] == 112
+		assert occ['f'] == 145
+		assert occ['i'] == 260
+		assert occ['h'] == 111
+		assert occ['k'] == 102
+		assert occ['j'] == 90
+		assert occ['m'] == 93
+		assert occ['l'] == 48
+		assert occ['o'] == 23
+		assert occ['n'] == 362
+		assert occ['p'] == 22
+
+	@mock.patch('scripts.config.get_conf_dir', return_value='res/')
+	def test_gen_key_check_field(self, mock1):
+		try:
+			gen = DataGenerator('char_gen_bad-01.json', seed='1234567890')
+			self.fail('Should have failed with KeyError on fieldName')
+		except KeyError as e:
+			assert("Could not find 'fieldName' in field of schema:" in str(e))
+			
+	@mock.patch('scripts.config.get_conf_dir', return_value='res/')
+	def test_gen_key_check_type(self, mock1):
+		try:
+			gen = DataGenerator('char_gen_bad-02.json', seed='1234567890')
+			self.fail('Should have failed with KeyError on type')
+		except KeyError as e:
+			assert('Could not find \'type\' in field of schema:' in str(e))
+			
+	@mock.patch('scripts.config.get_conf_dir', return_value='res/')
+	def test_gen_key_check_root(self, mock1):
+		try:
+			gen = DataGenerator('char_gen_bad-03.json', seed='1234567890')
+			self.fail('Should have failed with TypeError')
+		except TypeError as e:
+			assert('Root of JSON Schema is not a list' in str(e))
+
+		
+	@mock.patch('scripts.config.get_conf_dir', return_value='res/')
+	def test_gen_check_field_type(self, mock1):
+		try:
+			gen = DataGenerator('char_gen_bad-04.json', seed='1234567890')
+			self.fail('Should have failed with TypeError')
+		except RuntimeError as e:
+			assert('Field type was not found. Please change the field type or implement the datum correctly' in str(e))
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
