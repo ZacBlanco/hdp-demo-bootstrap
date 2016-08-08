3ba3e47a8382fbfaef36255759d10ea8fae87482
zblanco <zblanco@apache.org>
2016-08-08 14:13:02 -0700

    [#27] - Work towards full service implementation
    Decided that user shouldn’t have to implement the service - this goes
    hand in hand with #26 (the UI for the service). We build on top of that
    and add a few configuration parameters.

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -1,8 +1,7 @@
-import sys, util
+import sys, pwd, grp
 from resource_management import *
 from resource_management.core.exceptions import ComponentIsNotRunning
-from util.shell import Shell
-from util import config
+from subprocess import call
 reload(sys)
 sys.setdefaultencoding('utf8')
 
@@ -24,28 +23,56 @@ sys.setdefaultencoding('utf8')
 
 
 class Master(Script):
-	
-	
-	def install(self, env):
+    
+  def create_linux_user(self, user, group):
+    try:
+      pwd.getpwnam(user)
+    except KeyError:
+      Execute('adduser ' + user)
+    try:
+      grp.getgrnam(group)
+    except KeyError:
+      Execute('groupadd ' + group)
+
+  def install(self, env):
+    import params
+    print 'Install the Demo Service Master';
+    self.create_linux_user(params.demo_user, params.demo_group)
+    if params.demo_user != 'root':
+      Execute('cp /etc/sudoers /etc/sudoers.bak')        
+      Execute('echo "' + params.demo_user + '    ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers')
+      
+    Execute('mkdir -p '+ params.demo_pid_dir + ' ' + params.demo_log_dir)
+    Execute('chown -R ' + params.demo_user + ':' + params.demo_group + ' ' + params.demo_pid_dir)
+    Execute('chown -R ' + params.demo_user + ':' + params.demo_group + ' ' + params.demo_log_dir)
+    
+    # Ensure pip is instaled
+    Execute('sudo yum install python-pip')
+    Execute('pip install -r ' + '/'.join([params.demo_bin_dir, 'requirements.txt']))
+
+  def stop(self, env):
 		# Fill me in!
-		print 'Install the Demo Service Master';
-	
-	def stop(self, env):
+    import params
+    print 'Stop the Demo Service Master'
+    Execute('kill `cat ' + params.demo_pid_file + '`' )
+
+  def start(self, env):
+    print 'Start the Demo Service Master'
+    import params
 		# Fill me in!
-		print 'Stop the Demo Service Master';
-	
-	def start(self, env):
+    Execute('nohup python ' + params.demo_bin_dir +  '/demo-server.py &')
+    Execute('cp ' + params.demo_bin_dir + '/demo.pid ' + params.demo_pid_file)
+    
+  def status(self, env):
+    print 'Status of the Demo Service Master'
+    import params
 		# Fill me in!
-		print 'Start the Demo Service Master';
-	
-	def status(self, env):
+    check_process_status(params.demo_pid_file)
+    
+  def configure(self, env):
+    pass
 		# Fill me in!
-		# check_process_status(pid_file)
-		print 'Status of the Demo Service Master';
-	
-	def configure(self, env):
-		# Fill me in!
-		print 'Configure the Demo Service Master';
+		# print 'Configure the Demo Service Master';
 
 
 if __name__ == "__main__":
--- package/scripts/params.py
+++ package/scripts/params.py
@@ -1,9 +1,25 @@
-import os
 import sys, os, pwd, grp, signal, time, glob
-from resource_management import *
-from subprocess import call
+#from resource_management import *
+#from subprocess import call
+#
+## Shell of params.py
+##
+##
+## Define variables here which you can then use in master.py
+#
+#config = Script.get_config()
+#
+#demo_port = config['configurations']['demo-config']['demo.server.port']
+#ambari_user = config['configurations']['demo-config']['demo.ambari.user']
+#ambari_pass = config['configurations']['demo-config']['demo.ambari.password']
+#data_config = config['configurations']['demo-config']['demo.data.configuration']
 
-# Shell of params.py
-#
-#
-# Defin variables here which you can then use in master.py
+demo_user = 'demo'
+demo_group = 'demo'
+demo_pid_dir = '/var/run/demo'
+demo_pid_file = '/var/run/demo/demo.pid'
+demo_log_dir = '/var/log/demo'
+demo_log_file = '/var/log/demo.log'
+
+filedir = os.path.dirname(os.path.realpath(__file__))
+demo_bin_dir = filedir + '/../../demo-files'
